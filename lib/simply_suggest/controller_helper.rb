45bc29bcad4c77bd43a11516c57ed2474994f132
Nils Berenbold <nils.berenbold@gmail.com>
2015-11-01 22:15:50 +0100

    code cleanup

--- lib/simply_suggest/controller_helper.rb
+++ lib/simply_suggest/controller_helper.rb
@@ -8,18 +8,15 @@ module SimplySuggest
     # options : hash [optional]
     #
     def recommendations_for item, options = {}
-      limit     = options.delete(:limit) || 10
-      page      = options.delete(:page)  || 1
-      load      = options.delete(:load)
-      load      = SimplySuggest.config.autoload if load.nil?
+      options = options.reverse_merge(default_options)
 
       klass     = item.is_a?(Hash) ? item.delete(:class) : item.class.to_s.downcase
       object_id = item.is_a?(Hash) ? item.delete(:object_id) : item.id
 
-      data = request_object.object_types.send(klass).object.recommendations.page(page).limit(limit).get(object_id)
+      data = request_object.object_types.send(klass).object.recommendations.page(options[:page]).limit(options[:limit]).get(object_id)
       return [] if data["recommendations"].blank?
       ids = data["recommendations"].map { |d| d["recommendation_id"] }
-      return item.class.where(id: ids) if load
+      return item.class.where(id: ids) if options[:load]
       ids
     end
 
@@ -29,19 +26,15 @@ module SimplySuggest
     # options : hash [optional]
     #
     def user_recommendations user_id, options = {}
-      object_type = options.delete(:object_type) || options.delete(:class)
-      limit       = options.delete(:limit) || 10
-      page        = options.delete(:page)  || 1
-      load        = options.delete(:load)
-      load        = SimplySuggest.config.autoload if load.nil?
+      options = options.reverse_merge(default_options)
 
-      if object_type.present?
-        data = request_object.user.send(object_type).recommendations.page(page).limit(limit).get(user_id)
+      if options[:object_type].present?
+        data = request_object.user.send(options[:object_type]).recommendations.page(options[:page]).limit(options[:limit]).get(user_id)
       else
         data = request_object.user.recommendations.page(page).limit(limit).get(user_id)
       end
       return [] if data["recommendations"].blank?
-      return data["recommendations"].map { |d| d["object_type"].classify.constantize.where(id: d["recommendation_id"]).first }.reject(&:nil?) if load
+      return data["recommendations"].map { |d| d["object_type"].classify.constantize.where(id: d["recommendation_id"]).first }.reject(&:nil?) if options[:load]
       data["recommendations"].map { |d| { type: d["object_type"], id: d["recommendation_id"] } }
     end
 
@@ -51,26 +44,20 @@ module SimplySuggest
     # options : hash [optional]
     #
     def get_trending_objects klass, options = {}
-      limit = options.delete(:limit) || 10
-      page  = options.delete(:page) || 1
-      load  = options.delete(:load)
-      load  = SimplySuggest.config.autoload if load.nil?
+      options = options.reverse_merge(default_options)
 
       data = request_object.object_type.trending.send(klass).page(page).limit(limit).get
       return [] if data["trending"].blank?
-      return klass.classify.constantize.where(id: data["trending"].map { |d| d["external_id"] }) if load
+      return klass.classify.constantize.where(id: data["trending"].map { |d| d["external_id"] }) if options[:load]
       data["trending"].map { |d| { type: d["object_type"], id: d["external_id"] } }
     end
 
     def search_objects query, klass, options = {}
-      limit      = options.delete(:limit) || 10
-      page       = options.delete(:page) || 1
-      facets     = options.delete(:facets)
-      conditions = options.delete(:conditions)
+      options = options.reverse_merge(default_options)
 
-      request = request_object.object_type.search.send(klass).page(page).limit(limit).add_params(query: query)
-      request.add_param(:conditions, conditions) if conditions.present?
-      request.add_param(:facets, facets) if facets.present?
+      request = request_object.object_type.search.send(klass).page(options[:page]).limit(options[:limit]).add_params(query: query)
+      request.add_param(:conditions, options[:conditions]) if options[:conditions].present?
+      request.add_param(:facets, options[:facets]) if options[:facets].present?
 
       data = request.get
       return [] if data["objects"].blank?
@@ -81,6 +68,14 @@ module SimplySuggest
   protected
     def request_object
       @request_object ||= SimplySuggest::Request.new
+    end
+
+    def default_options
+      {
+        limit: 10,
+        page: 1,
+        load: SimplySuggest.config.autoload
+      }
     end
   end
 end
